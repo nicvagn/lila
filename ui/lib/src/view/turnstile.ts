369833655bcdc38cdd195a7a4c0f1fbcba0a762c
Thibault Duplessis <t@lichess.org>
2026-04-20 12:07:33 +0200

    fix signup form without turnstile element

--- ui/lib/src/view/turnstile.ts
+++ ui/lib/src/view/turnstile.ts
@@ -1,33 +1,37 @@
 import { requestIdleCallbackSafe } from '@/common';
 
+const selector = '.cf-turnstile';
+
 export default function turnstile($form: Cash): void {
-  $form.find('.submit').prop('disabled', true);
-  const selector = '.cf-turnstile';
-  const el = $form[0]!.querySelector<HTMLDivElement>(selector)!;
-  el.innerHTML = '';
-  const options = Object.assign({}, el.dataset);
-  const showError = (message: string | false) => {
-    const $err = $form.find('.cf-turnstile-error');
-    if (message) $err.html(`<p>${message}</p>`).removeClass('none');
-    else $err.addClass('none');
-  };
-  requestIdleCallbackSafe(() => {
-    window.turnstile.render(selector, {
-      ...options,
-      appearance: 'interaction-only',
-      callback: () => {
-        $form.find('.submit').prop('disabled', false);
-        showError(false);
-      },
-      'error-callback': (errorCode: string) => {
-        showError('Captcha error: ' + errorCode);
-      },
-      'expired-callback': () => {
-        showError('Captcha expired, please try again');
-      },
-      'timeout-callback': () => {
-        showError('Captcha timed out');
-      },
+  $form.find(selector).each(function (this: HTMLDivElement) {
+    const turnstileDiv = this;
+    $form.find('.submit').prop('disabled', true);
+    console.log($form, $form[0]);
+    turnstileDiv.innerHTML = '';
+    const options = Object.assign({}, turnstileDiv.dataset);
+    const showError = (message: string | false) => {
+      const $err = $form.find('.cf-turnstile-error');
+      if (message) $err.html(`<p>${message}</p>`).removeClass('none');
+      else $err.addClass('none');
+    };
+    requestIdleCallbackSafe(() => {
+      window.turnstile.render(selector, {
+        ...options,
+        appearance: 'interaction-only',
+        callback: () => {
+          $form.find('.submit').prop('disabled', false);
+          showError(false);
+        },
+        'error-callback': (errorCode: string) => {
+          showError('Captcha error: ' + errorCode);
+        },
+        'expired-callback': () => {
+          showError('Captcha expired, please try again');
+        },
+        'timeout-callback': () => {
+          showError('Captcha timed out');
+        },
+      });
     });
   });
 }
