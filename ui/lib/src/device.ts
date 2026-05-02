18262a171b73f9c87986d2202381cefae4e8c482
Paweł Pluta <56258596+PawelPlutaUek20@users.noreply.github.com>
2026-05-03 01:31:40 +1000

    lint: fix dependency cycle

--- ui/lib/src/device.ts
+++ ui/lib/src/device.ts
@@ -1,6 +1,6 @@
 import { memoize } from './index';
 import * as licon from './licon';
-import { bind, type Hooks } from './view';
+import { bind, type Hooks } from './view/snabbdom';
 
 export const hookMobileMousedown = (f: (e: Event) => any): Hooks =>
   bind('ontouchstart' in window ? 'click' : 'mousedown', f);
